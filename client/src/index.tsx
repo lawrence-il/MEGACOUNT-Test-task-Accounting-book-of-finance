d75f2a0a03e3ffe35d804f80072172ff95f1cdd5
Ilya <76962262+lawrence-il@users.noreply.github.com>
2022-09-01 06:42:37 +0500

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,17 +4,16 @@ import { state } from './store/store';
 import App from './components/app/App';
 import 'antd/dist/antd.min.css';
 import './index.sass';
+import { BrowserRouter } from 'react-router-dom';
 
 export const Context = createContext(state);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <Context.Provider value={state}>
-        <App />
-    </Context.Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Context.Provider>,
 );
-
-
